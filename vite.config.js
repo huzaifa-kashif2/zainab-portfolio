3f1f1d1f3f73878f20468a4172808b4a1ef47a23
huzaifa-kashif2 <huzaifa.kashif2@gmail.com>
2025-08-08 19:51:36 +0500

    Vite.config.js set

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  base: '/zainab-portfolio/',
+  base: '/',
   plugins: [react()],
 })
